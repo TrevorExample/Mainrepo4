8a44831e6e8e448527145e8abc95506cfd6c0d7d
man nguyen <man.nguyen@traviscountytx.gov>
2022-03-06 14:00:39 -0600

    adding new project

---
+++ APIMVC1/APIMVC1/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APIMVC1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+   
+        private static async Task<string> ProcessRepositories(string movieTitle)
+        {
+            var movieList = "";
+            var url = "http://www.omdbapi.com/?s="+ movieTitle + "&apikey=3f66eca5";
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                var result = await response.Content.ReadAsStringAsync();
+                movieList = result;
+            }
+            return movieList;
+        }
+
+            public async Task<ActionResult> CallAPIAsync(string searchValue)
+        {
+            var result = await ProcessRepositories(searchValue);
+            return Json(result);
+        }
+    }
+
+}
---
+++ APIMVC1/APIMVC1/Scripts/app.js
@@ -0,0 +1,54 @@
+﻿
+
+function updateMovieList(text) {
+    document.getElementById("movieResults").innerHTML = "";
+    const responseMessage = JSON.parse(JSON.parse(text));
+    
+
+    if (responseMessage["Response"] =="False") {
+        alert(responseMessage["Error"]);
+    }
+    else {
+        
+        var movies = responseMessage["Search"];
+    for (let i = 0; i < movies.length; i++) {
+        const node = document.createElement("li");
+        const textnode = document.createTextNode(movies[i].Title);
+        node.appendChild(textnode);
+        document.getElementById("movieResults").appendChild(node);
+        }
+    }
+
+}
+
+
+function callAPI() {
+
+    var searchInput = document.getElementById("searchInput").value;
+
+    if (searchInput == null || searchInput.length === 0 || searchInput === null || searchInput.match(/^ *$/) !== null) {
+        alert("search value is empty");
+    }
+    else {
+
+    var http = new XMLHttpRequest();
+    var url = '/Home/CallAPIAsync';
+    var params = 'searchValue=' + searchInput;
+    http.open('POST', url, true);
+
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    http.onreadystatechange = function () {
+        if (http.readyState == 4 && http.status == 200) {
+
+            updateMovieList(http.responseText);
+        }
+        else if(http.status !== 200){
+            alert("Problem retrieving data ");
+            console.log(http.responseText);
+        }
+    }
+    http.send(params);
+    }
+};
+
